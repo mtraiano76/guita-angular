4e39a5d242b955ebbc2611ae8b225a6d234c8401
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2022-08-10 09:35:21 -0300

    Input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title="Hola Miguel"
-  modificado=false
+  title = "Hola Miguel"
+  modificado = false
+  classInput = ""
+  op1 = 0
+  op2 = 0
+  resultado = 0
   constructor() { }
-  cambiarTitle(){
-    this.title="Hola Miguel"
-    this.modificado=true
+  cambiarTitle() {
+    if (this.title === "") {
+      this.classInput = "error"
+    } else {
+      this.classInput = "ok"
+    }
+    this.title = "Hola Miguel"
+    this.modificado = true
+  }
+  calcular() {
+    this.resultado = this.op1 + this.op2
   }
 
   ngOnInit(): void {
